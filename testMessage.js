de4ed37d3a16a01f06b7895a4944bc9d73b67517
Afnan Hassan <hassanafnan09@gmail.com>
2025-01-22 21:15:40 +0500

    Added the Tutoring Functionality

--- testMessage.js
+++
@@ -1,6 +0,0 @@
-const { sendMessage } = require('./utils/SMS.js');
-
-// Test the function
-sendMessage('+923464880929', 'This is a test message')
-    .then(() => console.log('Message sent successfully'))
-    .catch((error) => console.error('Error sending message:', error));
